4faf11ca77eda6ce5122ef66881e40fc30a4a87e
Otávio Carvalho <121265969+OtavioCarvalho73@users.noreply.github.com>
2023-10-12 23:19:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("scroll", () => {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+});
